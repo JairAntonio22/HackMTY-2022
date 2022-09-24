bcf3f870ca05575b3786941dbad13ecfface83ba
JairAntonio22 <jair.loranca@gmail.com>
2022-09-24 18:36:13 -0500

    Create model.py

---
+++ model.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('diabetes-dataset.csv')
+
+y = data['Outcome']
+x = data.drop(columns=['Outcome'])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10)
+
+models = [
+    DecisionTreeClassifier(),
+    KNeighborsClassifier(),
+    GaussianNB(),
+    LogisticRegression(solver='liblinear')
+]
+
+for model in models:
+    model.fit(x_train, y_train)
+    score = model.score(x_test, y_test)
+    y_pred = model.predict(x_test)
+    area = roc_auc_score(y_test, y_pred)
+    print('score', score)
+    print('area', area)
