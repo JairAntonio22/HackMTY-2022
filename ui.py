9ce8be43409eb7418efa7a91b643b08ada8cf483
Francisco Lazcano <francisco@pop-os.localdomain>
2022-09-24 21:52:51 -0500

    añadido tabs, funcionalidad de diabetes funciona al 100%

--- ui.py
+++ ui.py
@@ -1,15 +1,44 @@
 import streamlit as st
+from joblib import load
+import numpy as np
 
-st.title("Herramienta de diagnostico para diabetes")
+model_diabetes=load("model_diabetes.joblib")
 
-genero=st.radio('Genero',('Hombre','Mujer'))
-if genero=='Mujer':
-    embarazos=st.number_input('Numero de embarazos hasta la fecha',0)
-glucosa=st.number_input('Nivel de glucosa',0)
-presion_arterial=st.number_input('presion arterial',0)
-grosor=st.number_input('grosor de la piel',0)
-insulina=st.number_input('Nivel de insulina',0)
-altura=st.number_input('Altura:',0)
-peso=st.number_input('Peso',0)
+diabetes, cancer, derrame=st.tabs(['Diabetes','Cancer de Mama','Derrame cerebral'])
 
-bmi=peso/(altura**2)
+with diabetes:
+    st.title("Herramienta de diagnostico para diabetes")
+
+    genero=st.radio('Genero',('Hombre','Mujer'))
+    if genero=='Mujer':
+        embarazos=st.number_input('Numero de embarazos hasta la fecha',0)
+    else:
+        embarazos=0
+    edad=st.number_input('Edad',0)
+    glucosa=st.number_input('Nivel de glucosa',0)
+    presion_arterial=st.number_input('presion arterial',0)
+    grosor=st.number_input('grosor de la piel',0)
+    insulina=st.number_input('Nivel de insulina',0)
+    altura=st.number_input('Altura:',0)
+    peso=st.number_input('Peso',0)
+
+    if altura!=0:
+        bmi=peso/(altura**2)
+    else:
+        bmi=0
+
+    load=st.button("Calcular")
+
+    if load:
+        p=model_diabetes.predict(np.array([[embarazos,glucosa,presion_arterial,grosor,insulina,bmi,edad]]))
+        if p[0]==0:
+            r='Usted presenta riesgo de diabetes, por favor contacte a su medico'
+        else:
+            r='Nuestro modelo no encontro riesgo, sin embargo esta calculadora no es una prescripcion medica, si tiene dudas contacte a su medico'
+        st.info(r)
+
+with cancer:
+    'test'
+
+with derrame:
+    'test'
